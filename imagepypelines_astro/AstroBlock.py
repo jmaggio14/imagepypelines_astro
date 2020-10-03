99eda6e8f2192fc7989bdc0b5bf283fc96dce0fc
Jeff <jmaggio14@gmail.com>
2020-10-03 11:35:39 -0400

    fixes minor errors

--- imagepypelines_astro/AstroBlock.py
+++ imagepypelines_astro/AstroBlock.py
@@ -6,7 +6,7 @@ class AstroBlock(ip.Block):
     """Special Block made for astronomy with a predefined IO inputs and useful
     properties
 
-    """0
+    """
     def __init__(self):
         """instantiates the AstroBlock"""
         # NOTE: add default input types
--- imagepypelines_astro/starfind.py
+++ imagepypelines_astro/starfind.py
@@ -1,8 +1,6 @@
-from photoutils import DAOStarFinder, IRAFStarFinder
-
-
-from .AstroBlock import AstroBlock
-
-
-class BaseStarFinder(AstroBlock):
-    def __init__(self, fwhm, threshold)
+# from photoutils import DAOStarFinder, IRAFStarFinder
+# from .AstroBlock import AstroBlock
+#
+#
+# class BaseStarFinder(AstroBlock):
+#     def __init__(self, fwhm, threshold):
